8f20afe23c33f9a27a6bf774f2b898f109a5ff00
Jorko2000 <george1102@abv.bg>
2026-01-19 18:47:00 +0200

    PlanRepository.java

---
+++ PlanRepository.java
@@ -0,0 +1,11 @@
+package com.telecomsystem.repository;
+
+import com.telecomsystem.model.Plan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlanRepository extends JpaRepository<Plan, Long> {
+}
+
+
