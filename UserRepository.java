6d252f73d16d30adbf241d1d29729cedcc94ac44
Jorko2000 <george1102@abv.bg>
2026-01-19 18:47:34 +0200

    UserRepository.java

--- UserRepository.java
+++ UserRepository.java
@@ -7,28 +7,3 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
-
-PlanRepository.java
-
-package com.telecomsystem.repository;
-
-import com.telecomsystem.model.Plan;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlanRepository extends JpaRepository<Plan, Long> {
-}
-
-
-BillingRepository.java
-
-package com.telecomsystem.repository;
-
-import com.telecomsystem.model.Billing;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BillingRepository extends JpaRepository<Billing, Long> {
-}
