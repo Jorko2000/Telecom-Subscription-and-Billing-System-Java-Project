c74650c0e47ef0841e095b8297bd7ada0e2f5e03
Jorko2000 <george1102@abv.bg>
2026-01-31 14:28:24 +0200

    TelecomSystemApplication.java

---
+++ src/main/java/com/telecomsystem/TelecomSystemApplication.java
@@ -0,0 +1,12 @@
+package com.telecomsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelecomSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelecomSystemApplication.class, args);
+    }
+}
