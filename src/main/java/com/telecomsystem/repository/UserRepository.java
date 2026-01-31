dd65f6cb3285bd467787ad8c11b465e560639eee
Jorko2000 <george1102@abv.bg>
2026-01-31 14:35:54 +0200

    UserRepository.java

---
+++ src/main/java/com/telecomsystem/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package com.telecomsystem.repository;
+
+import com.telecomsystem.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
+
+PlanRepository.java
+
+package com.telecomsystem.repository;
+
+import com.telecomsystem.model.Plan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlanRepository extends JpaRepository<Plan, Long> {
+}
