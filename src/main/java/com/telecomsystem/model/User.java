0f564af5362873ca3988de0879cfa4dcf40689e4
Jorko2000 <george1102@abv.bg>
2026-01-31 14:37:28 +0200

    User.java

---
+++ src/main/java/com/telecomsystem/model/User.java
@@ -0,0 +1,22 @@
+package com.telecomsystem.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+
+    @ManyToOne
+    private Plan plan;
+}
